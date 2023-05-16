7a3c4912aa02e55b46a9c6ae2d6e40f52dace3a1
Kirill Rozhin <k-rozhin@mail.ru>
2023-05-16 16:11:24 +0300

    Вёрстка
    Сделана вёрстка для страницы авторизации

---
+++ Desktop_Auth/Desktop_Auth/Views/AuthorizationPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop_Auth.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_InputClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы вошли в ситему", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_CancelClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы отменили ввод", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_ReloadClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Обновление кода", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
