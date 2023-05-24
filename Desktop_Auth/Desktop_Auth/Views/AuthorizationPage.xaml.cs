017b1b0197913de34ead780406af61e8aa9498ee
Kirill Rozhin <k-rozhin@mail.ru>
2023-05-24 18:52:36 +0300

    Авторизация
    Реализована проверка номера сотрудника, пароля и кода доступа.
    Сделана рандомная генерация кода.

--- Desktop_Auth/Desktop_Auth/Views/AuthorizationPage.xaml.cs
+++ Desktop_Auth/Desktop_Auth/Views/AuthorizationPage.xaml.cs
@@ -16,31 +16,41 @@ using System.Windows.Shapes;
 
 namespace Desktop_Auth.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для AuthorizationPage.xaml
-    /// </summary>
     public partial class AuthorizationPage : Page
     {
+        private string accessCode; // Поле для хранения сгенерированного кода доступа
         public AuthorizationPage()
         {
             InitializeComponent();
         }
         private void Button_InputClick(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("Вы вошли в ситему", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
-            var CurrentUser = AppData.db.Authorization.FirstOrDefault(u => u.Number == TxbNumber.Text && u.Password == TxbPassword.Text);
-            if(CurrentUser != null)
+            string employeeNumber = TxbNumber.Text;
+            string password = TxbPassword.Text;
+
+            var currentUser = AppData.db.Authorization.FirstOrDefault(u => u.Number == TxbNumber.Text && u.Password == TxbPassword.Text);
+            if (currentUser != null && currentUser.Password == password)
             {
-                NavigationService.Navigate(new RolePage());
+                // Генерируем код доступа
+                accessCode = GenerateAccessCode();
+
+                // Открываем модальное окно с сгенерированным кодом доступа
+                MessageBox.Show($"Ваш код доступа: {accessCode}", "Код доступа", MessageBoxButton.OK, MessageBoxImage.Information);
+                
+                TxbCode.IsEnabled = true;
+                TxbCode.Focus();
             }
             else
             {
-                MessageBox.Show("Данного пользователя нет в базе");
+                MessageBox.Show("Неправильный номер сотрудника или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void Button_CancelClick(object sender, RoutedEventArgs e)
         {
+            TxbNumber.Text = "";     // Очистить поле ввода номера
+            TxbPassword.Text = "";   // Очистить поле ввода пароля
+            TxbCode.Text = "";
             MessageBox.Show("Вы отменили ввод", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
@@ -48,5 +58,66 @@ namespace Desktop_Auth.Views
         {
             MessageBox.Show("Обновление кода", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
         }
+
+        private void EmployeeNumberTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string employeeNumber = TxbNumber.Text;
+
+                // Проверяем наличие номера сотрудника в базе данных
+                if (CheckEmployeeNumber(employeeNumber))
+                {
+                    TxbPassword.IsEnabled = true;
+                    TxbPassword.Focus(); // Устанавливаем фокус на поле ввода пароля
+                }
+                else
+                {
+                    MessageBox.Show("Введённый номер не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void TxbCode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string enteredCode = TxbCode.Text;
+
+                if (enteredCode == accessCode)
+                {
+                    // Переходим на другую страницу после правильного ввода кода доступа
+                    NavigationService.Navigate(new RolePage());
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный код доступа", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private bool CheckEmployeeNumber(string employeeNumber)
+        {
+            var currentUser = AppData.db.Authorization.FirstOrDefault(u => u.Number == employeeNumber);
+            return currentUser != null;
+        }
+
+        private string GenerateAccessCode()
+        {
+            // Генерируем код доступа (8 символов, латиница, верхний и нижний регистр, спецсимвол, цифра)
+            string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+            Random random = new Random();
+            StringBuilder accessCodeBuilder = new StringBuilder();
+
+            for (int i = 0; i < 8; i++)
+            {
+                int index = random.Next(0, characters.Length);
+                accessCodeBuilder.Append(characters[index]);
+            }
+
+            return accessCodeBuilder.ToString();
+        }
     }
 }
+
+
